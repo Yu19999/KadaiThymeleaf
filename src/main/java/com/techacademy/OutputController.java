0cfc67cc231c45cc430a4d6c7682d806c65b311a
yu <yu912912@gmail.com>
2024-07-01 19:54:14 +0900

    ２回目

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class OutputController {
 
     @PostMapping("/output")
